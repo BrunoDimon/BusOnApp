032411dfa91e4d15eae5b35fe92b467575e5b95e
thiagoDimon <othiago.miranda@outlook.com>
2024-04-26 21:37:59 -0300

    Conecta api chat gpt

---
+++ src/service/api/requests/chatRequests.js
@@ -0,0 +1,10 @@
+import { api } from '../api';
+
+export const enviarMensagem = async (mensagem) => {
+    try {
+        return request = await api.post('/chatbot/conversation', {message: mensagem})
+        .then(response => response.data.response.content);
+    } catch(error) {
+        console.log(error)
+    }
+};
