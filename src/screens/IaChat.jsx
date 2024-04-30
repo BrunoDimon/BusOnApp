4ff748abfc61207f9147a0eac95911267795d68e
thiagoDimon <othiago.miranda@outlook.com>
2024-04-29 21:18:41 -0300

    Atribui animação de mensagem

--- src/screens/IaChat.jsx
+++ src/screens/IaChat.jsx
@@ -1,49 +1,56 @@
-import { Dimensions } from "react-native";
 import {
   Box,
-  Heading,
-  Pressable,
   Input,
   InputField,
   Button,
   ScrollView,
   Spinner,
-  HStack,
   VStack,
 } from "@gluestack-ui/themed";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { useSelector } from "react-redux";
 import CardMensagem from "../components/CardMensagem";
 import { enviarMensagem } from "../service/api/requests/chatRequests";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Text } from "@gluestack-ui/themed";
-
+import { TypingAnimation } from "react-native-typing-animation";
 
 export default IaChat = () => {
   const [inputChat, setInputChat] = useState();
   const [mensagens, setMensagens] = useState([]);
   const [awaitResponse, setAwaitResponse] = useState(false);
 
+  const scrollViewRef = useRef();
+  useEffect(() => {
+    scrollViewRef.current.scrollToEnd({ animated: true });
+  }, [mensagens]);
+
   async function enviarMensagemChat(mensagem) {
     if (mensagem) {
       setAwaitResponse(true);
       setInputChat(null);
-      setMensagens(mensagens => [...mensagens, {
-        mensagem: mensagem,
-        enviadoDe: "Você"
-      }]);
+      setMensagens((mensagens) => [
+        ...mensagens,
+        {
+          mensagem: mensagem,
+          enviadoDe: "Você",
+        },
+      ]);
       const data = await enviarMensagem(mensagem);
-      setMensagens(mensagens => [...mensagens, {
-        mensagem: data,
-        enviadoDe: "IA"
-      }]);
+      setMensagens((mensagens) => [
+        ...mensagens,
+        {
+          mensagem: data,
+          enviadoDe: "IA",
+        },
+      ]);
       setAwaitResponse(false);
     }
   }
 
   return (
     <Box flex={1}>
-      <Box flex={1}
+      <Box
+        flex={1}
         $dark-bg={"$backgroundDark900"}
         $light-bg={"$white"}
         alignItems={"start"}
@@ -56,33 +63,46 @@ export default IaChat = () => {
         justifyContent={"space-between"}
       >
         <Box flex={1} borderRadius={30}>
-          <ScrollView >
+          <ScrollView ref={scrollViewRef} showsVerticalScrollIndicator={false}>
             {mensagens?.map((mensagem, index) => {
               return (
-                <CardMensagem key={index} mensagem={mensagem.mensagem} enviadoDe={mensagem.enviadoDe} />)
-            })
-            }
+                <CardMensagem
+                  key={index}
+                  mensagem={mensagem.mensagem}
+                  enviadoDe={mensagem.enviadoDe}
+                />
+              );
+            })}
           </ScrollView>
         </Box>
         <Box flexGrow={0}>
-          {awaitResponse &&
-            (
-              <VStack alignItems="center">
-                <Spinner />
-                <Text>Buscando resposta...</Text>
-              </VStack>
-            )
-          }
+          {awaitResponse && (
+            <VStack alignItems="flex-start">
+              <TypingAnimation
+                dotColor="#ffdc72"
+                dotMargin={6}
+                dotAmplitude={6}
+                dotSpeed={0.18}
+                dotRadius={3}
+                dotX={20}
+                dotY={-14}
+              />
+            </VStack>
+          )}
 
-          <Input
-            variant="outline"
-            size="md"
-            mx={2}
-            mt={10}
-            borderRadius={18}
-          >
-            <InputField placeholder="Faça uma pergunta para a IA..." onChangeText={(v) => setInputChat(v)} value={inputChat} />
-            <Button isDisabled={awaitResponse || !inputChat} h={'$full'} px={8} bg={'transparent'} onPress={() => enviarMensagemChat(inputChat)} >
+          <Input variant="outline" size="md" mx={2} mt={10} borderRadius={18}>
+            <InputField
+              placeholder="Faça uma pergunta para a IA..."
+              onChangeText={(v) => setInputChat(v)}
+              value={inputChat}
+            />
+            <Button
+              isDisabled={awaitResponse || !inputChat}
+              h={"$full"}
+              px={8}
+              bg={"transparent"}
+              onPress={() => enviarMensagemChat(inputChat)}
+            >
               <MaterialCommunityIcons
                 name="send-circle-outline"
                 size={30}
