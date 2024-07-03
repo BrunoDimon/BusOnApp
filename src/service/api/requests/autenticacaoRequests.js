9807a948f2307fe6a73c43c6cf4fdab3c3b88806
DouglasKuerten <dk.kuerten.dk@hotmail.com>
2024-07-03 00:54:00 -0300

    - Correções Issue #24 - Ajustado para o token revalidar ao entrar na aplicação e o mesmo tiver vencido, além de forçar o logout quando ele não existir. - Ajustado erro que ocorria e crashava o app quando o token expirava e ficava sem nenhuma rota para o app ir, gerando um erro.

--- src/service/api/requests/autenticacaoRequests.js
+++ src/service/api/requests/autenticacaoRequests.js
@@ -1,4 +1,5 @@
-import { login, logout, refreshUser } from "../../../store/authSlice";
+import Toast from "react-native-root-toast";
+import { login, logout, refreshUser, refreshToken } from "../../../store/authSlice";
 import { store } from "../../../store/storeConfig";
 import { api } from "../api";
 
@@ -9,17 +10,28 @@ export const loginRequest = async (email, senha) => {
     return request = await api.post('/autenticacao/autenticar', data);
 }
 
-
-
-
 export const validateToken = async () => {
     await api.post('/autenticacao/validar-token')
         .then(response => {
             const dadosUsuario = response.data.user
             store.dispatch(refreshUser(dadosUsuario));
         })
-        .catch(error => {
-            store.dispatch(logout());
-            alert('Login expirado, deslogando...')
+        .catch(async (err) => {
+            console.log(err.response.data);
+            await api.post('/autenticacao/atualizar-token', { refreshToken: store.getState().auth.refreshToken })
+                .then((response) => {
+                    const dadosNovoToken = response.data;
+                    store.dispatch(refreshToken({ token: dadosNovoToken.accessToken, refreshToken: dadosNovoToken.refreshToken }));
+                    console.log('Token atualizado')
+                }).catch((error) => {
+                    console.log(error.response.data.error)
+                    let toast = Toast.show('Login expirado, deslogando...', {
+                        duration: Toast.durations.LONG,
+                    });
+                    setTimeout(function hideToast() {
+                        Toast.hide(toast);
+                    }, 5000);
+                    store.dispatch(logout());
+                })
         })
 }
